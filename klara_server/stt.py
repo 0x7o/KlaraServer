8c01a1666878f4c80316bcbd6d3ba2bb965513a3
Danil <me@0x7o.link>
2023-01-02 17:22:05 +0800

    init

---
+++ klara_server/stt.py
@@ -0,0 +1,35 @@
+import base64
+import whisper
+import numpy as np
+import soundfile as sf
+from config import Config
+
+
+class STT:
+    def __init__(self, config: Config):
+        self.config = config
+        self.model, self.options = self.load_model()
+
+    def load_model(self):
+        model = whisper.load_model(
+            self.config.get_config("whisper_model"),
+            device=self.config.get_config("whisper_device"),
+        )
+        options = whisper.DecodingOptions(
+            language=self.config.get_config("whisper_language"),
+            fp16=self.config.get_config("fp16"),
+        )
+        return model, options
+
+    def base64_to_wav(self, base64_string):
+        wav_bytes = base64.b64decode(base64_string)
+        wav = np.frombuffer(wav_bytes, dtype=np.int16)
+        sf.write("temp.wav", wav, self.config.get_config("sample_rate"))
+
+    def transcribe(self, base64_string):
+        self.base64_to_wav(base64_string)
+        audio = whisper.load_audio("temp.wav")
+        audio = whisper.pad_or_trim(audio)
+        mel = whisper.log_mel_spectrogram(audio).to(self.model.device)
+        transcript = whisper.decode(self.model, mel, self.options)
+        return transcript.text
---
+++ klara_server/tts.py
@@ -0,0 +1,37 @@
+import torch
+import base64
+import soundfile as sf
+from config import Config
+from IPython.display import Audio
+
+
+class TTS:
+    def __init__(self, config: Config):
+        self.config = config
+        model, text = torch.hub.load(
+            repo_or_dir="snakers4/silero-models",
+            model="silero_tts",
+            language=self.config.get_config("tts_language"),
+            speaker=self.config.get_config("tts_model"),
+        )
+        model.to(self.config.get_config("tts_device"))
+        self.model = model
+
+    def generate(self, text: str):
+        audio = self.model.apply_tts(
+            text=text,
+            speaker=self.config.get_config("tts_speaker"),
+            sample_rate=self.config.get_config("tts_sample_rate"),
+            put_accent=True,
+            put_yo=True,
+        )
+        audio = Audio(audio, rate=self.config.get_config("tts_sample_rate"))
+        base64_audio = base64.b64encode(audio.data)
+        return base64_audio
+
+
+if __name__ == "__main__":
+    config = Config("config.json")
+    tts = TTS(config)
+    base64_audio = tts.generate("Привет, я Клара")
+    print(base64_audio)
