e89ecc915ac74ef2d04b229f264c970ab28f9fee
Danil <danil.kononyuk@ya.ru>
2023-01-21 20:36:31 +0800

    nllb api replace

--- klara_server/nllb.py
+++ klara_server/nllb.py
@@ -1,29 +1,20 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 from config import Config
-import torch
+import requests
 
 
 class Translate:
     def __init__(self, config: Config):
         self.config = config
-        self.model, self.tokenizer = self.load_model()
-
-    def load_model(self):
-        model = AutoModelForSeq2SeqLM.from_pretrained(
-            self.config.get_config("nllb_model")
-        ).to(self.config.get_config("nllb_device"))
-        tokenizer = AutoTokenizer.from_pretrained(self.config.get_config("nllb_model"))
-        return model, tokenizer
 
     def transcribe(self, text, src_lang, tgt_lang, gender_name, gender_translation):
-        translator = pipeline(
-            "translation",
-            model=self.model,
-            tokenizer=self.tokenizer,
-            device=self.config.get_config("nllb_device_index"),
-            src_lang=src_lang,
-            tgt_lang=tgt_lang,
-        )
-        text = translator(f"{gender_name}: {text}", max_length=400)
-        text.replace(gender_translation, "")
-        return {"text": text}
+        url = "https://cloud.yandex.ru/api/translate/translate"
+        payload = {
+            "sourceLanguageCode": src_lang,
+            "targetLanguageCode": tgt_lang,
+            "texts": [f"{gender_name}: {text}"],
+        }
+        headers = {"Content-Type": "application/json"}
+        response = requests.request("POST", url, json=payload, headers=headers)
+        translation = response.json()["translations"][0]["text"]
+        translation = translation.replace(gender_translation, "")
+        return translation
