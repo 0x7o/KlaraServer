49f37445cd8b9b91171290ee4e28ec7d8b1b2edf
Danil <danil.kononyuk@ya.ru>
2023-01-22 14:25:58 +0800

    test

--- klara_server/stt.py
+++ klara_server/stt.py
@@ -11,9 +11,6 @@ class STT:
     def __init__(self, config: Config):
         self.config = config
         self.model, self.processor = self.load_model()
-        self.model.config.forced_decoder_ids = self.processor.get_decoder_prompt_ids(
-            language=self.config.get_config("whisper_language"), task="transcribe"
-        )
 
     def load_model(self):
         processor = WhisperProcessor.from_pretrained(
@@ -35,13 +32,14 @@ class STT:
         rate, data = read("temp.wav")
         input_speech = np.array(data, dtype=np.float32)
         input_speech = torch.from_numpy(input_speech)
+        self.model.config.forced_decoder_ids = self.processor.get_decoder_prompt_ids(
+            language=self.config.get_config("whisper_language"), task="transcribe"
+        )
         input_features = self.processor(
             input_speech,
             return_tensors="pt",
             sampling_rate=self.config.get_config("sample_rate"),
         ).input_features.to(self.config.get_config("whisper_device"))
         predicted_ids = self.model.generate(input_features)
-        transcription = self.processor.batch_decode(
-            predicted_ids, skip_special_tokens=True
-        )
+        transcription = self.processor.batch_decode(predicted_ids)
         return transcription[0]
